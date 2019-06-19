857b8bffd883f11fb0d9a0f221b7a284a8b02bae
Hua GAO <gaoh@pku.edu.cn>
2019-06-19 10:37:03 +0800

    add extra parameters to control svg `width` and `height` see issue #7

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ jQuery(function(){
 
 function draw (style){
 	var sel = 'pre.flowchartjs';
-	if (style) sel = sel + '.' + style;
+	if (style != '') sel = sel + '.' + style;
 	return function (opt){
 		jQuery(sel).each(function (index){
-			var classes = jQuery(this).attr('class');
-			var id = classes.replace(/ /g, '_') + '_' + index;
+			var cls = jQuery(this).attr('class').replace(/^\s+|\s+$/, '').replace(/\s+/g, ' ').split(' ');
 			var obj = flowchart.parse( jQuery(this).text() );
+			var classes = cls.length == 1? cls[0]: cls[0] + ' ' + cls[1];
+			var id = classes.replace(/ /g, '_') + '_' + index;
 			jQuery(this).replaceWith(
 				jQuery('<div class="' + classes + '" id="' + id + '"></div>')
 			);
 			obj.drawSVG(id, opt);
+			if (cls.length >= 3) jQuery('div#' + id + ' svg').attr('width', cls[2]);
+			if (cls.length >= 4) jQuery('div#' + id + ' svg').attr('height', cls[3]);
 		});
 	};
 }
@@ -33,11 +36,9 @@ function draw1style (style, callback){
 
 var classes = [];
 jQuery('pre.flowchartjs').each(function(index){
-	var cls = jQuery(this).attr('class');
-	cls = cls.replace(/flowchartjs/, '');
-	cls = cls.replace(/^\s+/, '');
-	cls = cls.split(' ')[0];
-	classes[cls] = 1;
+	var cls = jQuery(this).attr('class').replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(' ');
+	if (cls.length == 1) classes[''] = 1;
+	else classes[cls[1]] = 1;
 	return true;
 });
 
